831e9f4bd90fb6ddf8dd67bdff204e6f03c6e2e2
xluo <406738397@qq.com>
2016-01-17 01:14:57 +0800

    完成了又拍云上传品牌logo

--- admin.shop.com/Application/Admin/Controller/UploadController.class.php
+++ admin.shop.com/Application/Admin/Controller/UploadController.class.php
@@ -12,13 +12,20 @@ class UploadController extends Controller
     public function index()
     {
 //        dump($_FILES);
-        //获取用户指定的文件上传到的文件夹
+        //获取浏览器指定的服务(空间
         $dir=I("post.dir");
         $config = array(
-            'rootPath'     => './Uploads/', //保存根路径
-            'savePath'     => $dir.'/', //保存路径
-            'driver'=>'',
-            'driverConfig'=>array(),
+//            'rootPath'     => './Uploads/', //保存根路径
+//            'savePath'     => $dir.'/', //保存路径
+            'rootPath'     => './', //保存到upyun的根路径
+            'driver'=>'Upyun',
+            'driverConfig' => array(
+                'host'     => 'v0.api.upyun.com', //又拍云服务器
+                'username' => 'xluo', //又拍操作员用户
+                'password' => 'lshaulu0.', //又拍云操作员密码
+                'bucket'   => $dir, //空间名称
+                'timeout'  => 90, //超时时间
+            ), // 上传驱动配置
         );
         $upload=new Upload($config);
         $rst=$upload->uploadOne($_FILES['Filedata']);
